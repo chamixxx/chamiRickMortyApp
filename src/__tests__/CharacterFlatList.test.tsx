3d7743e4c770682877e2de7a4f216f2a4306beae
Othmane Chami khazraji <othmane.chami@gmail.com>
2018-11-23 09:45:18 +0100

    Last unit test for testin flat list rendering

--- src/__tests__/CharacterFlatList.test.tsx
+++ src/__tests__/CharacterFlatList.test.tsx
@@ -12,9 +12,10 @@ import Adapter from "enzyme-adapter-react-16";
 import { Provider } from "react-redux";
 import { FlatList, ActivityIndicator } from "react-native";
 import { SearchBar } from "react-native-elements";
+import Character from "../Models/Character";
 configure({ adapter: new Adapter() });
 
-describe("renders correctly", () => {
+describe("renders characterFlatList Correctly correctly", () => {
   //   it("basic test", () => {
   //     const store = mockStore({ characters: initialCharactersState });
   //     let component = shallow(
@@ -28,13 +29,51 @@ describe("renders correctly", () => {
   //     expect(component).toBeTruthy();
   //     expect(component.instance().getListOrIndicator).toBeCalled();
   //   });
-  it("renders all children", () => {
-    const store = mockStore({ characters: initialCharactersState });
+  it("renders all children when is not loading", () => {
+    const store = mockStore({
+      characters: initialCharactersState
+    });
     //@ts-ignore
     const wrapper = shallow(<CharacterFlatList store={store} />);
     const component = shallow(wrapper.getElements()[0]);
     expect(component.find(FlatList).length).toEqual(1);
     expect(component.find(SearchBar).length).toEqual(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("renders all children when is  loading", () => {
+    const store = mockStore({
+      characters: { ...initialCharactersState, isLoading: true }
+    });
+    //@ts-ignore
+    const wrapper = shallow(<CharacterFlatList store={store} />);
+    const component = shallow(wrapper.getElements()[0]);
+    expect(component.find(SearchBar).length).toEqual(1);
+    expect(component.find(ActivityIndicator).length).toEqual(1);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("data in flat list equal data in state", () => {
+    const store = mockStore({
+      characters: {
+        ...initialCharactersState,
+        charactersArray: [
+          new Character(),
+          new Character(),
+          new Character(),
+          new Character()
+        ]
+      }
+    });
+    //@ts-ignore
+    const wrapper = shallow(<CharacterFlatList store={store} />);
+    const component = shallow(wrapper.getElements()[0]);
+    expect(
+      component
+        .find(FlatList)
+        .at(0)
+        .props().data.length
+    ).toEqual(store.getState().characters.charactersArray.length);
     expect(component).toMatchSnapshot();
   });
 });
